e345f230086b1fd9f6d54d170170213e9b16b577
CedricoBanjo <femmininocedric@yahoo.fr>
2025-06-01 14:49:15 +0200

    Update App V2.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { LiFiWidget } from '@lifi/widget';
 
 const widgetConfig = {
-  integrator: 'realtswap',
   fee: {
     recipient: '0xc64C27E0b7407b0ae6c87329aAf5bb0cAd76BF4f',
-    amount: '0.001', //0.1
+    amount: '0.001', // correspond à 0.1%
     feeType: 'percentage',
   },
   theme: {
@@ -20,7 +19,7 @@ function App() {
   return (
     <div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div style={{ width: '500px', height: '600px' }}>
-        <LiFiWidget config={widgetConfig} />
+        <LiFiWidget integrator="realtswap" config={widgetConfig} />
       </div>
     </div>
   );
