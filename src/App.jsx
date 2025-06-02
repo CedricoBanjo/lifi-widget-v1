f8707942645982658e004a48445bf9bc17c939fb
CedricoBanjo <femmininocedric@yahoo.fr>
2025-06-02 11:03:47 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { LiFiWidget } from '@lifi/widget';
 
 const widgetConfig = {
-  fee: {
-    recipient: '0xc64C27E0b7407b0ae6c87329aAf5bb0cAd76BF4f',
-    amount: 0.001, // correspond à 0.1%
-    feeType: 'percentage',
+  integrator: 'realtswap',
+  containerStyle: {
+    border: '1px solid #eee',
+    borderRadius: '16px',
   },
-  theme: {
-    container: {
-      border: '1px solid #eee',
-      borderRadius: '16px',
+  options: {
+    fee: {
+      recipient: '0xc64C27E0b7407b0ae6c87329aAf5bb0cAd76BF4f',
+      amount: 0.001, // 0.1%
+      feeType: 'percentage',
     },
   },
 };
@@ -19,7 +20,7 @@ function App() {
   return (
     <div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div style={{ width: '500px', height: '600px' }}>
-        <LiFiWidget integrator="realtswap" config={widgetConfig} />
+        <LiFiWidget config={widgetConfig} />
       </div>
     </div>
   );
