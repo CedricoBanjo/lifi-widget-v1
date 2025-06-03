325e1e59f2fdecd4e8c8d38d25ba9004d44b8d2d
CedricoBanjo <femmininocedric@yahoo.fr>
2025-06-03 12:48:31 +0200

    Update App.jsx - amélioration

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { LiFiWidget } from '@lifi/widget';
 
 const widgetConfig = {
-  integrator: 'realtswap',
   variant: 'wide',
   containerStyle: {
     border: '1px solid #eee',
@@ -11,11 +10,17 @@ const widgetConfig = {
   options: {
     fee: {
       recipient: '0xc64C27E0b7407b0ae6c87329aAf5bb0cAd76BF4f',
-      amount: 0.001,
+      amount: 0.001, // 0.1%
       feeType: 'percentage',
     },
   },
   appearance: 'light',
+  walletConfig: {
+    onConnect: () => {
+      console.log('Wallet connected!');
+    },
+    usePartialWalletManagement: true,
+  },
   theme: {
     colorSchemes: {
       light: {
@@ -105,7 +110,7 @@ function App() {
 
       {/* Widget */}
       <div style={{ width: '100%', maxWidth: '500px', height: '600px' }}>
-        <LiFiWidget config={widgetConfig} />
+        <LiFiWidget integrator="realtswap" config={widgetConfig} />
       </div>
     </div>
   );
